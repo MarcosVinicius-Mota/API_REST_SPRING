10979ed7bfcdf923a72a93312e5659d7e70cfbe6
MarcosVinicius-Mota <marcosvinicius.nota652790@gmail.com>
2023-10-25 21:48:10 -0300

    Path Params

---
+++ src/main/java/com/teste/demo/Greeting.java
@@ -0,0 +1,13 @@
+package com.teste.demo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Greeting {
+
+    private Long id;
+    private String content;
+}
